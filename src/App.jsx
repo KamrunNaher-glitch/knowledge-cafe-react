5f4244cfeee320a4f96695159b82238a9351d389
KamrunNaher-glitch <keyachandani7@gmail.com>
2025-02-11 20:26:09 +0600

    display blog data

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
@@ -10,7 +11,13 @@ function App() {
   return (
     <>
      <Header></Header>
-     <Blogs></Blogs>
+      <div className='md:flex  w-11/12 mx-auto'>
+      <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      </div>
+
+
+    
       
      
      
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 
 const Blogs = () => {
@@ -10,8 +11,11 @@ const Blogs = () => {
         .then(data => setBlogs(data))
     },[])
     return (
-        <div>
-            
+        <div className="md:w-2/3">
+            <h1>Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog =><Blog key={blog.id} blog={blog}></Blog>)
+            }
         </div>
     );
 };
